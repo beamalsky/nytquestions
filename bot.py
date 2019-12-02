acc89036fe18b8b8d6e27ee07a6bee70607d6017
beamalsky <beamalsky@gmail.com>
2019-12-02 10:53:56 -0600

    Reset LOCAL_DEVELOPMENT

--- bot.py
+++ bot.py
@@ -5,7 +5,7 @@ from pprint import pprint
 
 from nytquestions import get_questions
 
-LOCAL_DEVELOPMENT = True
+LOCAL_DEVELOPMENT = False
 
 if LOCAL_DEVELOPMENT:
     from secrets import *
@@ -24,8 +24,6 @@ auth.set_access_token(ACCESS_KEY, ACCESS_SECRET)
 api = tweepy.API(auth)
 
 questions = get_questions(NYT_API_KEY)
-print("Questions found: ")
-pprint(questions)
 
 for question in questions:
     q = str(question)
